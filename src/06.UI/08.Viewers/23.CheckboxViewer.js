ef1f33f9c36389096a397e40c02a26d46cd6dbe2
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-06-24 17:22:32 +0300

    -

---
+++ src/06.UI/08.Viewers/23.CheckboxViewer.js
@@ -0,0 +1,32 @@
+Colibri.UI.CheckboxViewer = class extends Colibri.UI.Viewer {
+
+    constructor(name, container, element = '<span />', root = null) {
+        super(name, container, element, root);
+        this.AddClass('app-bool-viewer-component');
+
+        this._checkbox = new Colibri.UI.Checkbox(this.name + '_check', this);
+        this._checkbox.shown = true;
+        this._checkbox.readonly = true;
+    }
+
+    set value(value) {
+        this._checkbox.readonly = false;
+        this._checkbox.checked = value;
+        this._checkbox.readonly = true;
+    }
+
+    get value() {
+        return this._checkbox.checked ?? null;
+    }
+
+    get field() {
+        return this._field;
+    }
+
+    set field(field) {
+        this._field = field;
+    }
+
+
+}
+Colibri.UI.Viewer.Register('Colibri.UI.CheckboxViewer', '#{app-viewers-checkbox;Галочка}');
