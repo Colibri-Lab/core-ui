b8dde13a64cc96dc7c0c9e2eaf4f580a235a940c
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-09-06 13:56:40 +0300

    -

--- src/06.UI/06.Forms/06.Number.js
+++ src/06.UI/06.Forms/06.Number.js
@@ -12,6 +12,8 @@ Colibri.UI.Forms.Number = class extends Colibri.UI.Forms.Field {
         }
         this._input = contentContainer.container.append(Element.create('input', params));
 
+        this._input.addEventListener('focus', (e) => this.Dispatch('ReceiveFocus', {domEvent: e}));
+        this._input.addEventListener('blur', (e) => this.Dispatch('LoosedFocus', {domEvent: e}));
         this._input.addEventListener('change', (e) => this.Dispatch('Changed', {domEvent: e}));
         this._input.addEventListener('keyup', (e) => this.Dispatch('KeyUp', {domEvent: e}));
         this._input.addEventListener('keydown', (e) => this.Dispatch('KeyDown', {domEvent: e}));
