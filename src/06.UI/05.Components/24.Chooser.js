683c8b0436a9b90a2b2815c4a9fe1a1c6e0f8f9c
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-09-12 16:29:39 +0300

    -

---
+++ src/06.UI/05.Components/24.Chooser.js
@@ -0,0 +1,289 @@
+Colibri.UI.Chooser = class extends Colibri.UI.Component {
+
+    _skipLooseFocus;
+    _itemSelected;
+    /**
+     * Конструктор
+     * @param {string} name название компонента
+     * @param {Element|string|Colibri.UI.Component} container 
+     * @param {boolean} multiple мультивыбор 
+     * @param {boolean} readonly только для чтения 
+     * @param {string[]|null} values значения 
+     * @param {string|number|null} defaultValue значение по умолчанию 
+     * @param {string} titleField название поля для отображения 
+     * @param {string} valueField название поля для значения 
+     * @param {Function|null} __render метод отрисовки
+     * @param {boolean} allowEmpty разрешено пустое значение
+     * @param {boolean} clearIcon показать clearIcon
+     */
+    constructor(name, container, multiple = false, readonly = true, placeholder = '', selector = null, defaultValue = null, allowEmpty = true, clearIcon = false) {
+        super(name, container, '<div />');
+
+        this.AddClass('app-chooser-component');
+
+        this._multiple = multiple;
+        this._selector = selector;
+        this._titleField = this._selector?.title || 'title';
+        this._valueField = this._selector?.value || 'value';
+        this._default = defaultValue;
+        if(typeof this._selector?.chooser == 'string') {
+            this._chooser = this._selector?.chooser ? eval(this._selector?.chooser) : null;
+        }
+        else if(typeof this._selector?.chooser == 'function') {
+            this._chooser = this._selector?.chooser ? this._selector?.chooser : null;
+        }
+        this._value = [];
+        this._placeholder = placeholder || '#{app-selector-nothingchoosed;Ничего не выбрано}';
+
+        this._input =  new Colibri.UI.Input(this._name + '-input', this);
+        this._input.shown = true;
+        this._input.icon = null;
+        this._input.hasIcon = false;
+        this._input.hasClearIcon = (clearIcon || !readonly);
+        this._input.placeholder = this._placeholder;
+
+        this._arrow = Element.create('span', {class: 'arrow'});
+        this._arrow.html(Colibri.UI.ChooserIcon);
+        this._element.append(this._arrow);
+
+        this.readonly = readonly;
+        this.allowempty = allowEmpty;
+
+        this._setValue(this._default);
+        this._renderValue();
+
+        this._handleEvents();
+    }
+
+    /**
+     * Регистрация событий
+     */
+    _registerEvents() {
+        super._registerEvents();
+
+        this.RegisterEvent('Changed', false, 'Когда выбор изменился');
+        this.RegisterEvent('ChooserClicked', false, 'Когда нажали на кнопку выбора');
+    }
+
+    _showChooser() {
+        if(this._chooser) {
+            const component = this._chooser;
+            this._chooserObject = new component(this.name + '-chooser', document.body, this._selector?.params || {});
+            this._chooserObject.AddHandler('Choosed', (event, args) => {
+                this.value = args.value;
+            });
+            this._chooserObject.Show();
+        } 
+        else {
+            this.Dispatch('ChooserClicked', {currentValue: this._value});
+        }
+        return false; 
+    }
+
+    /**
+     * Регистрация обработчиков событий
+     */
+    _handleEvents() {
+
+        this._input.AddHandler('KeyUp', (event, args) => this.Dispatch('KeyUp', args));
+
+        this._input.AddHandler('Filled', (event, args) => this.__Filled(event, args));
+        this._input.AddHandler('Cleared', (event, args) => this.__Cleared(event, args));
+
+        this._arrow.addEventListener('click', (e) => {
+            this._showChooser();
+            return false; 
+        });
+
+        // перехватить keydown и обработать Escape
+        this._input.AddHandler('KeyDown', (event, args) => {
+
+            if(['Enter', 'NumpadEnter'].indexOf(args.domEvent.code) !== -1) {
+                this._showChooser();
+                args.domEvent.stopPropagation();
+                args.domEvent.preventDefault();
+                return false;
+            }
+
+
+            return this.Dispatch('KeyDown', args);        
+        });
+
+    }
+
+    __Filled(event, args) {
+        let v = {};
+        v[this._valueField] = this._input.value;
+        super.value = v;
+    }
+
+    __Cleared(event, args) {
+        this._setValue(null);
+        this.Dispatch('Changed', args);
+        this._renderValue();
+
+        args.domEvent?.preventDefault();
+        args.domEvent?.stopPropagation();
+        return false;
+    }
+
+    /**
+     * Выбранное значение
+     * @return array|Object|boolean
+     */
+    get value() {
+        return this._value;
+    }
+
+    /**
+     * Установить выбранное значение
+     * @param {*} value
+     */
+    set value(value) {
+        this._setValue(value);
+        this._renderValue();
+    }
+
+    set multiple(value) {
+        this._multiple = value === 'true' || value === true;
+    }
+
+    get multiple() {
+        return this._multiple;
+    }
+
+
+    /**
+     * Установить выбранное значение
+     * @param {Object|Array} value
+     */
+    _setValue(value) {
+
+        if( value && this.multiple && !Array.isArray(value) ) {
+            throw 'Value must be array of an object/string, becose it is multiple';
+        }
+        this._value = value;
+    }
+
+    /**
+     * Отобразить значения
+     */
+    _renderValue() {
+        // this._value = [string] | [{title, value}] | null
+        if(!this._value) {
+            this._input.value = '';
+            return;
+        }
+        
+        if (!this.multiple) {
+            this._input.value = this._value instanceof Object ? (this._value[this._titleField] ?? this._value[this._valueField] ?? '') : this._value;
+        } else {
+            const values = this._value.map((v) => v instanceof Object ? (v[this._titleField] ?? v[this._valueField] ?? '') : v);
+            this._input.value = values.join(', ');
+        }
+    }
+
+    /**
+     * Поставить фокус
+     */
+    Focus() {
+        this._input.Focus();
+    }
+
+
+    /**
+     * Свойство только для чтения
+     */
+    get readonly() {
+        return !!this._input.readonly;
+    }
+    set readonly(value) {
+        if(value === true || value === 'true') {
+            this.AddClass('app-component-readonly');
+            this._input.readonly = true;
+        }
+        else {
+            this.RemoveClass('app-component-readonly');
+            this._input.readonly = false;
+        }
+    }
+
+    /**
+     * Текст для отображения вместо значения
+     */
+    get placeholder() {
+        return this._placeholder;
+    }
+    set placeholder(value) {
+        this._placeholder = value;
+        this._input.placeholder = this._placeholder;
+        this._renderValue(false);
+    }
+
+    get placeholderinfo() {
+        return this._placeholderinfo;
+    }
+    set placeholderinfo(value) {
+        this._placeholderinfo = value;
+        this._renderValue(false);
+    }
+
+    get placeholderempty() {
+        return this._placeholderempty;
+    }
+    set placeholderempty(value) {
+        this._placeholderempty = value;
+    }
+
+    /**
+     * Вкючено/выключено
+     */
+    get enabled() {
+        return this._input.enabled;
+    }
+    set enabled(value) {
+        if(value) {
+            this.RemoveClass('app-component-disabled');
+        }
+        else {
+            this.AddClass('app-component-disabled');
+        }
+        this._input.enabled = value;
+    }
+
+
+    /**
+     * Индекс табуляции
+     * @todo проверить правильно ли получаю tabIndex и исправить
+     * @type {number}
+     */
+    get tabIndex() {
+        return this._input && this._input.tabIndex;
+    }
+    set tabIndex(value) {
+        if (this._input) {
+            this._input.tabIndex = value;
+        }
+    }
+
+}
+
+Colibri.UI.Chooser.ChooseWindow = class extends Colibri.UI.Window {
+
+    constructor(name, container, element, title, width, height) {
+        super(name, container, element, title, width, height);
+    }
+
+    _registerEvents() {
+        this.RegisterEvent('Choosed', false, 'Когда выбор сделан');
+    }
+
+    get params() {
+        return this._params;
+    }
+    set params(value) {
+        this._params = value;
+    }
+    
+}
+
---
+++ src/06.UI/06.Forms/24.Choose.js
@@ -0,0 +1,342 @@
+Colibri.UI.Forms.Choose = class extends Colibri.UI.Forms.Field {
+
+    /**
+     * Отрисовка содержания компонента поля 
+     */
+    RenderFieldContainer() {
+
+        this.AddClass('app-component-choose-field');
+
+
+        const contentContainer = this.contentContainer;
+
+        this._input = this._createSelector();
+
+        this._initializeValues();
+
+        this._input.shown = true;
+        this.placeholder = this._fieldData.placeholder;
+        if(this._fieldData?.params?.placeholderinfo) {
+            this.placeholderinfo = this._fieldData.params.placeholderinfo;
+        }
+
+        this._input.AddHandler('Changed', (event, args) => this.Dispatch('Changed', args));
+        this._input.AddHandler('KeyDown', (event, args) => this.Dispatch('KeyDown', args));
+        this._input.AddHandler('KeyUp', (event, args) => this.Dispatch('KeyUp', args));
+        this._input.AddHandler('Clicked', (event, args) => this.Dispatch('Clicked', args));
+
+        if(this._fieldData?.params?.readonly === undefined) {
+            this.readonly = false;    
+        }
+        else {
+            this.readonly = this._fieldData?.params?.readonly;
+        }
+        if(this._fieldData?.params?.enabled === undefined) {
+            this.enabled = true;
+        }
+        else {
+            this.enabled = this._fieldData.params.enabled;
+        }
+
+        if(this._fieldData?.selector?.ondemand) {
+            this._input.__BeforeFilled = () => {
+                return new Promise((resolve, reject) => {
+                    if (this._fieldData.lookup) {
+                        this.loading = true;
+                        this.AddClass('app-select-loading');
+                        this._setLookup(this._fieldData.lookup).then((response) => {
+                            this.values = response.result || response;
+                        }).finally(() => {
+                            this.loading = false;                        
+                            this.RemoveClass('app-select-loading');
+                            this._setEnabled();
+                            resolve(true);
+                        });
+                    }
+                });
+            };
+        }
+
+    }
+
+    /**
+     * Заново загрузить значения из хранилища
+     */
+    ReloadValues() {
+        this.values = this._fieldData.values;
+        if (this._fieldData.lookup) {
+            this.loading = true;
+            this.AddClass('app-select-loading');
+            this._setLookup(this._fieldData.lookup).then((response) => {
+                this.values = response.result || response;
+            }).finally(() => {
+                this.loading = false;
+
+                this.value = this.value ? this._input._findValue(this.value) : (this._fieldData.default ?? null);
+
+                this.RemoveClass('app-select-loading');
+                this._setEnabled();
+                this.Dispatch('Changed');
+            });
+        }
+    }
+
+    _getDependsValue(type = null) {
+        if (this.root && this._fieldData?.lookup) {
+
+            if((type && !this._fieldData.lookup[type]['depends']) || (!type && !this._fieldData.lookup['depends'])) {
+                return;
+            }
+
+            let dependsField = type ? this._fieldData.lookup[type]['depends'] : this._fieldData.lookup['depends'];
+            if (dependsField) {
+                const rootValues = this.root?.value;
+                if(eval(`typeof rootValues?.${dependsField}`) !== 'undefined') {
+                    return eval(`rootValues.${dependsField}`);
+                }
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Установить новое значение свойству lookup
+     * Загрузить значения селектора альтернативным способом, указанным в lookup
+     * @param {(Object|function)} value
+     */
+    _setLookup(value) {
+        let lookupPromise;
+        this._lookup = value;
+
+        if (typeof this._lookup == 'function' || typeof this._lookup == 'string') {
+            if(typeof this._lookup == 'string') {
+                this._lookup = eval(this._lookup);
+            }
+
+            let dependsValue = this._getDependsValue();
+            let dependsField = this._lookup.depends ?? null;
+    
+            const lookupMethodRun = this._lookup(dependsValue, dependsField);
+            lookupPromise = lookupMethodRun instanceof Promise ? lookupMethodRun : new Promise((resolve, reject) => {
+                resolve({
+                    result: this._lookup()
+                });
+            });
+        }
+        else if (typeof this._lookup == 'object') {
+
+            if(this._lookup?.method) {
+                let lookupMethod = this._lookup.method;
+                if (typeof lookupMethod == 'string') {
+                    lookupMethod = eval(this._lookup.method);
+                }
+
+                if(typeof lookupMethod !== 'function') {
+                    lookupPromise = new Promise((resolve, reject) => { resolve({result: ''}); })
+                }
+                else {
+                    let dependsValue = this._getDependsValue();
+                    let dependsField = this._lookup.depends ?? null;   
+                    lookupPromise = lookupMethod(this._input._input.value, dependsValue, dependsField);
+                }
+            }
+            else if(this._lookup?.binding) {
+                let binding = this._lookup.binding;
+                if (typeof binding == 'string') {
+                    let dependsValue = this._getDependsValue('binding');
+                    lookupPromise = App.Store.AsyncQuery(binding, dependsValue);
+                }
+            }
+            else if(this._lookup?.controller) {
+                let controller = this._lookup.controller;
+                let module = eval(controller.module);
+                let dependsValue = this._getDependsValue('controller');
+                let dependsField = this._lookup.controller.depends ?? null;
+                lookupPromise = module.Call(controller.class, controller.method, {term: this._input._input.value, param: dependsValue, depends: dependsField, lookup: this._lookup});
+            }
+            else if(this._lookup?.storage) {
+                let controller = this._lookup?.storage?.controller;
+                let module = eval(controller?.module);
+                let dependsValue = this._getDependsValue('storage');
+                let dependsField = this._lookup?.storage?.depends ?? null;
+                lookupPromise = module.Call(controller.class, controller.method, {term: this._input._input.value, param: dependsValue, depends: dependsField, lookup: this._lookup});
+            }
+            else {
+                lookupPromise = new Promise((resolve, reject) => { resolve({result: ''}); })
+            }
+        }
+
+        // каждый метод должен возвращать промис
+        return lookupPromise;
+    }
+
+    /**
+     * Значения селектора
+     * @param {array} value
+     * */
+    set values(value) {
+        let required = this._fieldData?.params?.required;
+        if(required === undefined) {
+            required = false;
+        }
+        if(!required) {
+            const o = {};
+            o[this._fieldData?.selector?.title] = '---';
+            o[this._fieldData?.selector?.value] = 0;
+            value = isIterable(value) ? [o, ...value] : [o];
+        }
+        this._input.values = value;
+    }
+
+    /**
+     * Значения селектора
+     * @return {array} value
+     * */
+    get values() {
+        return this._input.values;
+    }
+
+    /**
+     * Поставить фокус
+     */
+    Focus() {
+        this._input.Focus();
+    }
+
+    /**
+     * Только для чтения
+     */
+    get readonly() {
+        return this._input.readonly;
+    }
+    set readonly(value) {
+        this._input.readonly = value;
+    }
+
+    /**
+     * Замещающий текст
+     */
+    get placeholder() {
+        return this._input.placeholder;
+    }
+    set placeholder(value) {
+        this._input.placeholder = value;//this._input._placeholder placeholder по умолчанию
+    }
+
+    get placeholderinfo() {
+        return this._input.placeholderinfo;
+    }
+    set placeholderinfo(value) {
+        this._input.placeholderinfo = value;
+    }
+
+    get placeholderempty() {
+        return this._input.placeholderempty;
+    }
+    set placeholderempty(value) {
+        this._input.placeholderempty = value;
+    }
+
+    /**
+     * Выбранное значение
+     */
+    get value() {
+        let value = this._lastValue || this._input.value;
+        if(this._fieldData?.params?.emptyAsNull && !value) {
+            value = null;
+        }
+        if(Array.isArray(value)) {
+            value = value.map((v) => v[this._fieldData.selector?.value ?? 'value'] ?? v);
+        }
+        else if(value instanceof Object) {
+            value = value[this._fieldData.selector?.value ?? 'value'] ?? value;
+        }
+        return value;
+    }
+    set value(value) {
+        if(this.loading === true) {
+            this._lastValue = value;
+        } else {
+            this._input.value = value;
+        }
+    }
+
+    /**
+     * Включен/выключен
+     */
+    get enabled() {
+        return this._input.enabled;
+    }
+    set enabled(value) {
+        this._input.enabled = value;
+    }
+
+    _setEnabled() {
+        if(!this.value && this._fieldData.default) {
+            this.value = this._fieldData.default;
+        }
+    }
+
+    /**
+     * Если необходимо инициализировать данные из lookup
+     * @private
+     */
+    _initializeValues() {
+        if (this._fieldData.lookup) {
+            this.loading = true;
+            this.AddClass('app-select-loading');
+
+            //delay для того чтобы в форму были загружены значения зависимых полей
+            Colibri.Common.Delay(this.root ? 100 : 0).then(() => {
+                this._setLookup(this._fieldData.lookup).then((response) => {
+                    this.values = response.result || response;
+                }).finally(() => {
+                    this.loading = false;
+                    if(this._lastValue) {
+                        this.value = this._lastValue;
+                        this._lastValue = null;
+                    } else {
+                        this._input._renderValue(false);
+                    }
+                    this.RemoveClass('app-select-loading');
+                    this._setEnabled();
+                });
+            });
+        } else {
+            this._setEnabled();
+        }
+    }
+
+    /**
+     * Индекс табуляции
+     * @todo проверить правильно ли получаю tabIndex и исправить
+     * @type {number}
+     */
+    get tabIndex() {
+        return this._input && this._input.tabIndex;
+    }
+    set tabIndex(value) {
+        if (this._input) {
+            this._input.tabIndex = value;
+        }
+    }
+
+    set popupconfig(value) {
+        this._input.popupconfig = value;
+    }
+
+    _createSelector() {
+        return new Colibri.UI.Chooser(
+            'input',
+            this.contentContainer,
+            this._fieldData.multiple ?? this._fieldData?.params?.multiple ?? false,
+            this._fieldData.readonly,
+            this._fieldData.placeholder,
+            this._fieldData.selector,
+            this._fieldData.default,
+            (this._fieldData.allowempty === undefined ? true : this._fieldData.allowempty),
+            (this._fieldData.clearicon === undefined ? false : this._fieldData.clearicon)
+        );
+    }
+}
+Colibri.UI.Forms.Field.RegisterFieldComponent('Select', 'Colibri.UI.Forms.Select', '#{app-fields-select;Выборка}')
