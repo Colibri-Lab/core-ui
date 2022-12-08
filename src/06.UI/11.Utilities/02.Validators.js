b6aeb8b1851a305cdd3ce5bf4b68c1cf745c0bdd
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-12-08 10:39:27 +0300

    -

--- src/06.UI/11.Utilities/02.Validators.js
+++ src/06.UI/11.Utilities/02.Validators.js
@@ -1,7 +1,7 @@
 Colibri.UI.Utilities.Validator = class {
 
     static IsUKPostCode(string) {
-        var postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i; 
+        var postcodeRegEx = /[A-Z]{1,2}[0-9A-Z]{1,2} ?[0-9][A-Z]{2}/i; 
         return postcodeRegEx.test(string); 
     }
 
