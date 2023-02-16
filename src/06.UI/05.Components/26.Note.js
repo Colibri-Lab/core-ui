aa91877ccad616a3b377d4cb5b99c9e9612cfec4
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-02-16 12:31:38 +0300

    -

---
+++ src/06.UI/05.Components/26.Note.js
@@ -0,0 +1,12 @@
+Colibri.UI.Note = class extends Colibri.UI.Pane {
+    
+    constructor(name, container) {
+        /* создаем компонент и передаем шаблон */
+        super(name, container);
+        this.AddClass('app-component-note');
+
+
+
+    }
+
+}
