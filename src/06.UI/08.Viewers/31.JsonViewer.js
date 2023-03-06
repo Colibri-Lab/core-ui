ca35d652c5c926e009443fc1f24ac28a1d9c40c4
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-03-06 22:18:53 +0300

    -

--- src/06.UI/08.Viewers/31.JsonViewer.js
+++ src/06.UI/08.Viewers/31.JsonViewer.js
@@ -25,28 +25,31 @@ Colibri.UI.JsonViewer = class extends Colibri.UI.Pane {
         this._showValue();
     }
     _showValue() {
-        console.log(this._value);
+
         if(Array.isArray(this._value)) {
-            for(let index=0; index<this._value.length;index++) {
-                const value = this._value[index];
-                const v = new Colibri.UI.JsonViewer(this.name + index, this);
-                v.shown = true;
-                v.value = value;
+            if(this._value.length > 0) {
+                for(let index=0; index<this._value.length;index++) {
+                    const value = this._value[index];
+                    const v = new Colibri.UI.JsonViewer(this.name + index, this);
+                    v.shown = true;
+                    v.value = value;
+                }
             }
         } else if(this._value instanceof Object) {
-            const fieldsViewer = new Colibri.UI.FieldsViewer(this.name + 'viewer', this);
-            fieldsViewer.shown = true;
-            
             const keys = Object.keys(this._value);
-            const fields = {};
-            for(const k of keys) {
-                fields[k] = {
-                    desc: k,
-                    component: this._value[k] instanceof Object ? 'Json' : 'Text'
-                };
+            if(keys.length > 0) {
+                const fieldsViewer = new Colibri.UI.FieldsViewer(this.name + 'viewer', this);
+                const fields = {};
+                for(const k of keys) {
+                    fields[k] = {
+                        desc: k,
+                        component: this._value[k] instanceof Object ? 'Json' : 'Text'
+                    };
+                }
+                fieldsViewer.shown = true;
+                fieldsViewer.fields = fields;
+                fieldsViewer.value = this._value;
             }
-            fieldsViewer.fields = fields;
-            fieldsViewer.value = this._value;
 
         }
         
