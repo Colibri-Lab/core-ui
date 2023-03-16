4de78384e2aaf4e16b807ce1f55ec3d27bb38687
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-03-16 16:10:28 +0300

    -

--- src/06.UI/03.Containers/08.Router.js
+++ src/06.UI/03.Containers/08.Router.js
@@ -14,6 +14,62 @@ Colibri.UI.Router = class extends Colibri.UI.Pane {
     }
 
     /**
+     * Route structure Object
+     * @type {Object}
+     */
+    get structure() {
+        return this._structure;
+    }
+    /**
+     * Route structure Object
+     * @type {Object}
+     */
+    set structure(value) {
+        this._structure = this.toPlain(value instanceof Object ? value : eval(value), '/');
+        this._initStructure();
+    }
+    _initStructure() {
+        
+        for(const pattern of Object.keys(this._structure)) {
+            const route = this._structure[pattern];
+
+            let component = null;
+            if(route?.component ?? null) {
+                const componentObject = route.component;
+                component = new componentObject(route?.name ?? ('component-' + Date.Mc()), this);
+            } else {
+                component = new route('component-' + Date.Mc(), this);
+            }
+
+            component.routePattern = pattern.substring(this.basePattern.length);
+
+            if(route?.attrs ?? null) {
+                Object.forEach(route?.attrs, (attrName, attrValue) => {
+                    component[attrName] = attrValue;
+                });
+            }
+
+            component.Disconnect();
+            component.shown = true;
+        }
+
+
+    }
+
+    toPlain(object, prefix = '') {
+        let ret = {};
+        for(const name of Object.keys(object)) {
+            const value = object[name];
+            ret[prefix + name] = value;
+            if(value?.childs ?? null) {
+                ret = Object.assign(ret, this.toPlain(value.childs, prefix + name + '/'));
+                delete value.childs;
+            }
+        }
+        return ret;
+    }
+
+    /**
      * 
      * @type {}
      */
