9d7723b8ed342e7fc9cc75091c897701af547ede
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-05-06 13:43:18 +0300

    -

--- src/06.UI/02.Common/11.Hr.js
+++ src/06.UI/02.Common/11.Hr.js
@@ -1,6 +1,6 @@
 Colibri.UI.Hr = class extends Colibri.UI.Component {
     constructor(name, container) {
-        super(name, container, '<hr />');
+        super(name, container, Element.create('hr'));
         this.AddClass('app-component-hr');
     }
 
