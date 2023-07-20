8b8887a1c41dd34c523f8127ba37cf608dbbbdcf
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-07-20 10:14:59 +0300

    -

--- src/06.UI/08.Viewers/09.TextViewer.js
+++ src/06.UI/08.Viewers/09.TextViewer.js
@@ -3,6 +3,22 @@ Colibri.UI.TextViewer = class extends Colibri.UI.Viewer {
     constructor(name, container, element = null, root = null) {
         super(name, container, element || Element.create('span'), root);
         this.AddClass('app-text-viewer-component');
+        this._emptyMessage = '&mdash;'
+    }
+
+    /**
+     * Message if empty
+     * @type {String}
+     */
+    get emptyMessage() {
+        return this._emptyMessage;
+    }
+    /**
+     * Message if empty
+     * @type {String}
+     */
+    set emptyMessage(value) {
+        this._emptyMessage = value;
     }
 
     get value() {
@@ -10,7 +26,7 @@ Colibri.UI.TextViewer = class extends Colibri.UI.Viewer {
     }
 
     set value(value) {
-        super.value = !value ? '&mdash;' : value;
+        super.value = !value ? this._emptyMessage : value;
     }
 
 
