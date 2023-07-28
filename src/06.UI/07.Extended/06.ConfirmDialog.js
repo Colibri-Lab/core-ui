1234b77c4fb8d4de676e82ccb86ef1bc81ca3a23
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-07-28 16:51:03 +0300

    -

--- src/06.UI/07.Extended/06.ConfirmDialog.js
+++ src/06.UI/07.Extended/06.ConfirmDialog.js
@@ -15,12 +15,13 @@ Colibri.UI.ConfirmDialog = class extends Colibri.UI.Window {
      * Показывает диалог
      * @param {Function(dialogResult)} callback результат диалога, true - да, false - нет 
      */
-    Show(title, message, button) {
+    Show(title, message, okbutton = null, cancelbutton = null) {
     
         return new Promise((resolve, reject) => {
             this.title = title;
             this.Children('message').value = message;
-            this.Children('btn-save').value = button || '#{ui-confirm-ok}';
+            this.Children('btn-save').value = okbutton || '#{ui-confirm-ok}';
+            this.Children('btn-cancel').value = cancelbutton || '#{ui-confirm-cancel}';
             super.Show();
 
             this.Children('btn-save').ClearHandlers();
