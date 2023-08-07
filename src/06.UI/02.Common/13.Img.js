69153b81b710e6927a6d078d94de5278e433b406
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-08-07 18:38:27 +0300

    -

---
+++ src/06.UI/02.Common/13.Img.js
@@ -0,0 +1,30 @@
+Colibri.UI.Img = class extends Colibri.UI.Component {
+
+    constructor(name, container) {
+        super(name, container, Element.create('img'));
+        this.AddClass('app-component-img');
+    }
+
+    get source() {
+        return this._element.attr('src');
+    }
+
+    set source(value) {
+        this._element.attr('src', value);
+
+    }
+
+    /**
+     * @type {String}
+     */
+    get alt() {
+        return this._element.attr('alt');
+    }
+    /**
+     * @type {String}
+     */
+    set alt(value) {
+        this._element.attr('alt', value);
+   }
+
+}
