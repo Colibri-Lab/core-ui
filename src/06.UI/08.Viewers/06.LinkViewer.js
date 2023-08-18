d73a78e46e3b3ed8cff655ebdfd8097a44d096ff
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-08-18 11:01:56 +0300

    -

--- src/06.UI/08.Viewers/06.LinkViewer.js
+++ src/06.UI/08.Viewers/06.LinkViewer.js
@@ -1,14 +1,20 @@
 Colibri.UI.LinkViewer = class extends Colibri.UI.Viewer {
 
     constructor(name, container, element = null, root = null) {
-        super(name, container, element || Element.create('span'), root);
+        super(name, container, element || Element.create('a'), root);
         this.AddClass('app-link-viewer-component');
     }
 
-    set downloadlink(value) {
-        this._downloadlink = value;
+    get value() {
+        return this._value;
     }
-
+    set value(value) {
+        value = this._convertValue(value);
+        this._value = value;
+        this._element.attr('target', this._field?.params?.target ?? '_self');
+        this._element.attr('href', value);
+        this._element.html(value);
+    }
 
 }
 Colibri.UI.Viewer.Register('Colibri.UI.LinkViewer', '#{ui-viewers-link}');
