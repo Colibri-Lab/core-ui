ba205d612db7d1f39fe060d413ebdc4214c25df8
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-08-23 13:43:24 +0300

    -

---
+++ src/06.UI/02.Common/14.Strong.js
@@ -0,0 +1,8 @@
+Colibri.UI.Strong = class extends Colibri.UI.Component {
+
+    constructor(name, container) {
+        super(name, container, Element.create('strong'));
+        this.AddClass('app-component-strong');
+    }
+
+}
---
+++ src/06.UI/02.Common/15.Hr.js
@@ -0,0 +1,8 @@
+Colibri.UI.Hr = class extends Colibri.UI.Component {
+
+    constructor(name, container) {
+        super(name, container, Element.create('strong'));
+        this.AddClass('app-component-hr');
+    }
+
+}
