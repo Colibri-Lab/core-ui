42fb545ede288c40d3d69db1b127b978d4e98621
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-09-05 17:17:14 +0300

    -

---
+++ src/06.UI/08.Viewers/34.DateDiffViewer.js
@@ -0,0 +1,30 @@
+Colibri.UI.DateDiffViewer = class extends Colibri.UI.Viewer {
+    
+    constructor(name, container) {
+        /* создаем компонент и передаем шаблон */
+        super(name, container);
+        this.AddClass('colibri-ui-daterangeviewer');
+
+        this._value = null;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        if(!Array.isArray(value)) {
+            value = [value, (new Date()).toDbDate()];
+        }
+
+        this._value = value;
+    
+        try {
+            super.value = value[0].toDate().DiffFullTokens(value[1].toDate());
+        } catch(e) {
+            console.log(e);
+            super.value = '';
+        }
+
+    }
+}
