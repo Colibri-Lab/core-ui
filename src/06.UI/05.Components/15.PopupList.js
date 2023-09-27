dfd74b86b4a290eb493c4d050a0bd9a68514a4b1
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-09-27 16:27:28 +0300

    -

--- src/06.UI/05.Components/15.PopupList.js
+++ src/06.UI/05.Components/15.PopupList.js
@@ -91,7 +91,7 @@ Colibri.UI.PopupList = class extends Colibri.UI.List {
         this.ClearAllGroups();
         this.ClearSelection(false);
 
-        if(!Object.isObject(value)) {
+        if(!Array.isArray(value)) {
             return;
         }
 
