f17427e991fdaca8c77e05f7f3490f7479acee05
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2024-02-26 14:06:12 +0300

    -

--- src/06.UI/04.Views/01.List.js
+++ src/06.UI/04.Views/01.List.js
@@ -210,7 +210,7 @@ Colibri.UI.List = class extends Colibri.UI.Component {
         let selected = null;
         this.ForEach((name, group) => {
             group.ForEach((n, item) => {
-                if((item.value['id'] ?? item.value) == (value['id'] ?? value)) {
+                if((item.value?.id ?? item.value) == (value?.id ?? value)) {
                     selected = item;
                     return false;
                 }
