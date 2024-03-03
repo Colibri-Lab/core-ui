55e310b28aeef9f480fa7f398ca02aa44f90aa46
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2024-03-03 14:52:51 +0300

    -

--- src/01.Common/01.Cookie.js
+++ src/01.Common/01.Cookie.js
@@ -1,40 +1,27 @@
 Colibri.Common.Cookie = class {
     
     static Set(c_name, value, exdays, path, domain, secure, samesite) {
-
-        if(App.Device.isIOs && WKWebViewCookies) {
-            WKWebViewCookies.setCookie({
-                name: c_name,
-                value: value,
-                domain: domain
-            });
-        } else {
-            var exdate = new Date();
-            exdate.setDate(exdate.getDate() + exdays);
-            var c_value = encodeURIComponent(value) + 
-                ((exdays==null) ? "" : "; expires=" + exdate.toUTCString()) + 
-                (path == null ? '; path=/' : '; path=' + path) + 
-                (domain ? '; domain=' + domain : '') + 
-                (secure !== undefined && secure ? '; secure' : '') + 
-                (samesite !== undefined ? '; SameSite=' + samesite : '');
-            document.cookie = c_name + "=" + c_value;    
-        }
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        var c_value = encodeURIComponent(value) + 
+            ((exdays==null) ? "" : "; expires=" + exdate.toUTCString()) + 
+            (path == null ? '; path=/' : '; path=' + path) + 
+            (domain ? '; domain=' + domain : '') + 
+            (secure !== undefined && secure ? '; secure' : '') + 
+            (samesite !== undefined ? '; SameSite=' + samesite : '');
+        document.cookie = c_name + "=" + c_value;
     }
 
     static Get (c_name) {
-        if(App.Device.isIOs && WKWebViewCookies) {
-            return WKWebViewCookies[c_name];
-        } else {
-            var i,x,y,ARRcookies=document.cookie.split(";");
-            for( i=0; i < ARRcookies.length; i++) {
-                x = ARRcookies[i].substring(0, ARRcookies[i].indexOf("="));
-                y = ARRcookies[i].substring(ARRcookies[i].indexOf("=") + 1);
-                x = x.replace(/^\s+|\s+$/g,"");
-                if(x == c_name)
-                    return decodeURIComponent(y);
-            }
-            return null;    
+        var i,x,y,ARRcookies=document.cookie.split(";");
+        for( i=0; i < ARRcookies.length; i++) {
+            x = ARRcookies[i].substring(0, ARRcookies[i].indexOf("="));
+            y = ARRcookies[i].substring(ARRcookies[i].indexOf("=") + 1);
+            x = x.replace(/^\s+|\s+$/g,"");
+            if(x == c_name)
+                return decodeURIComponent(y);
         }
+        return null;
     }
 
     static Delete(c_name, path, domain) {
