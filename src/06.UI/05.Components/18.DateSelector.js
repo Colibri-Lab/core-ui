9d016a90744ad91359280d5161b9fd31c92b8e54
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2024-11-05 14:38:12 +0400

    -

--- src/06.UI/05.Components/18.DateSelector.js
+++ src/06.UI/05.Components/18.DateSelector.js
@@ -31,6 +31,12 @@ Colibri.UI.DateSelector = class extends Colibri.UI.Component {
 
         this._hiddenElement.addEventListener('click', (e) => {this.Dispatch('Clicked', { domEvent: e }); e.preventDefault(); e.stopPropagation(); return false;});
         this._hiddenElement.addEventListener('change', (e) => {
+            if(this.value < this._min) {
+                this.value = this._min;
+            } else if(this.value > this._max) {
+                this.value = this._max;
+            }
+            
             this._showValue();
             if(this._changeTimeout) {
                 clearTimeout(this._changeTimeout);
