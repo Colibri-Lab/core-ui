51ea30e8cf0e4c5581c56d741af0aeb038d1b9ed
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-06-05 11:08:51 +0400

    -

--- src/02.Common/14.Video.js
+++ src/02.Common/14.Video.js
@@ -9,47 +9,123 @@ Colibri.Common.Video = class {
     _videoObject = null;
 
     RecordVideo(videoComponent, videoSettings = null, audioSettings = null, dataReceivedCallback = null) {
+        const selectedMic = App.Browser.Get('selected-mic-id');
+        const selectedCamera = App.Browser.Get('selected-camera-id');
+
+        if(selectedCamera) {
+            if(videoSettings) {
+                videoSettings.deviceId = { exact: selectedCamera };
+            } else {
+                videoSettings = {deviceId: { exact: selectedCamera }};
+            }
+        }
+
+        if(selectedMic) {
+            if(audioSettings) {
+                audioSettings.deviceId = { exact: selectedMic };
+            } else {
+                audioSettings = {deviceId: { exact: selectedMic }};
+            }
+        }
+
         this._videoObject = videoComponent;
-        return this._startRecordVideoOnWeb(videoComponent, videoSettings, audioSettings, dataReceivedCallback);
-    }
-
-    _startRecordVideoOnWeb(videoComponent, videoSettings = null, audioSettings = null, dataReceivedCallback = null) {
         return new Promise((resolve, reject) => {
-            navigator.mediaDevices.getUserMedia({ audio: audioSettings ?? true, video: videoSettings ?? true })
-                .then(stream => {
-                    videoComponent.srcObject = stream;
-                    videoComponent.play();
 
-                    this._stream = stream;
-                    this._mediaRecorder = new MediaRecorder(stream);
-                    const audioChunks = [];
+            if(selectedCamera === 'screen') {
 
-                    this._mediaRecorder.addEventListener("dataavailable", event => {
-                        audioChunks.push(event.data);
+                delete audioSettings.deviceId;
+                videoSettings = { displaySurface: 'window' };
+                const displayMediaOptions = {
+                    video: true,
+                    audio: audioSettings ?? true,
+                };
+
+                navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+                    .then(stream => {
+                        videoComponent.srcObject = stream;
+                        videoComponent.play();
+    
+                        this._stream = stream;
+                        this._mediaRecorder = new MediaRecorder(stream);
+                        const audioChunks = [];
+    
+                        this._mediaRecorder.addEventListener("dataavailable", event => {
+                            audioChunks.push(event.data);
+                        });
+    
+                        this._mediaRecorder.addEventListener("stop", () => {
+                            const videoBlob = new Blob(audioChunks, { type: 'video/webm' });
+                            resolve(videoBlob);
+                        });
+    
+                        this._mediaRecorder.addEventListener("start", () => {
+                            if(dataReceivedCallback) {
+                                dataReceivedCallback();
+                            }
+                        });
+    
+                        this._mediaRecorder.start();
+    
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        return null;
                     });
+            } else {
 
-                    this._mediaRecorder.addEventListener("stop", () => {
-                        const videoBlob = new Blob(audioChunks, { type: 'video/webm' });
-                        resolve(videoBlob);
+                navigator.mediaDevices.getUserMedia({ audio: audioSettings ?? true, video: videoSettings ?? true })
+                    .then(stream => {
+                        videoComponent.srcObject = stream;
+                        videoComponent.play();
+    
+                        this._stream = stream;
+                        this._mediaRecorder = new MediaRecorder(stream);
+                        const audioChunks = [];
+    
+                        this._mediaRecorder.addEventListener("dataavailable", event => {
+                            audioChunks.push(event.data);
+                        });
+    
+                        this._mediaRecorder.addEventListener("stop", () => {
+                            const videoBlob = new Blob(audioChunks, { type: 'video/webm' });
+                            resolve(videoBlob);
+                        });
+    
+                        this._mediaRecorder.addEventListener("start", () => {
+                            if(dataReceivedCallback) {
+                                dataReceivedCallback();
+                            }
+                        });
+    
+                        this._mediaRecorder.start();
+    
+                    })
+                    .catch(error => {
+                        reject(error);
                     });
+            }
 
-                    this._mediaRecorder.addEventListener("start", () => {
-                        if(dataReceivedCallback) {
-                            dataReceivedCallback();
-                        }
-                    });
-
-                    this._mediaRecorder.start();
-
-                })
-                .catch(error => {
-                    reject(error);
-                });
         });
     }
 
+
     StopRecording() {
-        return this._stopRecordVideooOnWeb();
+        if(this._audioContext) {
+            this._audioContext?.close();
+        }
+        if(this._mediaRecorder) {
+            this._mediaRecorder.stop();
+            this._mediaRecorder = null;
+        }
+        if(this._stream) {
+            this._stream.getTracks().forEach((track) => {
+                if (track.readyState == 'live') {
+                    track.stop();
+                }
+            });
+            this._stream = null;
+        }
+        return Promise.resolve();
     }
     
     CaptureScreeshot(secondstowait = 3) {
@@ -70,23 +146,5 @@ Colibri.Common.Video = class {
         });
     }
 
-    _stopRecordVideooOnWeb() {
-        if(this._audioContext) {
-            this._audioContext?.close();
-        }
-        if(this._mediaRecorder) {
-            this._mediaRecorder.stop();
-            this._mediaRecorder = null;
-        }
-        if(this._stream) {
-            this._stream.getTracks().forEach((track) => {
-                if (track.readyState == 'live') {
-                    track.stop();
-                }
-            });
-            this._stream = null;
-        }
-        return Promise.resolve();
-    }
 
 }
