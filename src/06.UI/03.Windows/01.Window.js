ad335850951e13202c01a6009843f8603e728752
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-08-15 02:46:56 +0300

    -

--- src/06.UI/03.Windows/01.Window.js
+++ src/06.UI/03.Windows/01.Window.js
@@ -89,8 +89,8 @@ Colibri.UI.Window = class extends Colibri.UI.Component {
 
             this.moving = true;
             this._movingPoint = {
-                left: clientX - this._element[0].getBoundingClientRect().left,
-                top: clientY - this._element[0].getBoundingClientRect().top
+                left: clientX - this._element.getBoundingClientRect().left,
+                top: clientY - this._element.getBoundingClientRect().top
             };
             
             if (this._state === 'minimized') {
