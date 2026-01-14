ee7d81aab688f5259447bc3f93cb3baf2f3ba715
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-01-14 13:56:48 +0400

    -

--- src/03.IO/05.WebSocketStream.js
+++ src/03.IO/05.WebSocketStream.js
@@ -1,22 +1,73 @@
 Colibri.IO.WebSocketStream = class extends Destructable {
 
-    constructor(uri, chunkSize, chunkCallback) {
+    static TYPE_READERS = {
+        Date: (dv, o, le) => new Date(dv.getFloat64(o, le)),
+        Float64: (dv, o, le) => dv.getFloat64(o, le),
+        Float32: (dv, o, le) => dv.getFloat32(o, le),
+        Uint32: (dv, o, le) => dv.getUint32(o, le),
+        Uint16: (dv, o, le) => dv.getUint16(o, le),
+        Uint8: (dv, o) => dv.getUint8(o)
+    };
+
+    static ARRAY_VIEWS = {
+        Float32Array,
+        Float64Array,
+        Uint32Array,
+        Uint16Array,
+        Uint8Array
+    };
+
+    /**
+     * constructor
+     * @constructor
+     * @param {String} uri websocket uri
+     * @param {Number|null} chunkSize part size in bytes or null (means that must be one row in chunk)
+     * @param {Array<Array>} format of chunk [[8,'time','Date'],[4,'duration','Uint8'],[['n',4], 'chunk','Float32Array']]
+     */
+    constructor(uri, chunkSize, chunkFormatter, chunkReceived) {
         super();
 
         this._uri = uri;
-        this._chunkCallback = chunkCallback;
+        this._chunkReceived = chunkReceived;
         this._chunkSize = chunkSize;
+        this._chunkFormatter = chunkFormatter;
         this._connect();
     }
 
-    _unpack(view, size) {
-        const result = [];
-        let i = 0;
-        while (i < view.length) {
-            result.push(view.slice(i, i + size));
-            i += size;
+    _format(buffer, layout) {
+        const dv = new DataView(buffer);
+        let offset = 0;
+        const out = {};
+
+        for (const [size, name, type] of layout) {
+
+            // variable length field
+            if (Array.isArray(size)) {
+                const stride = size[1];
+                const bytesLeft = buffer.byteLength - offset;
+                const count = bytesLeft / stride;
+                const Ctor = Colibri.IO.WebSocketStream.ARRAY_VIEWS[type];
+
+                out[name] = new Ctor(buffer, offset, count);
+                offset += bytesLeft;
+                break;
+            }
+
+            // scalar
+            out[name] = Colibri.IO.WebSocketStream.TYPE_READERS[type](dv, offset, littleEndian);
+            offset += size;
         }
-        return result;
+
+        return out;
+    }
+
+    _unpackRows(buffer, rowSize) {
+        const rows = [];
+        const total = buffer.byteLength;
+        for (let offset = 0; offset < total; offset += rowSize) {
+            rows.push(buffer.slice(offset, offset + rowSize));
+        }
+        return rows;
     }
 
     _connect() {
@@ -24,11 +75,18 @@ Colibri.IO.WebSocketStream = class extends Destructable {
         this._socket.binaryType = "arraybuffer";
         this._socket.onopen = (event) => console.log('WebSocket connection opened:', event);
         this._socket.onmessage = (event) => {
-            let chunk = new Float32Array(event.data);;
-            if(this._chunkSize && chunk.length > this._chunkSize) {
-                chunk = this._unpack(chunk, this._chunkSize);
+            let chunks = event.data;
+            if (this._chunkSize && chunks.length > this._chunkSize) {
+                chunks = this._unpackRows(chunks, this._chunkSize);
+            } else {
+                chunks = [chunks];
             }
-            this._chunkCallback(chunk);
+            if (this._chunkFormatter) {
+                for (let i = 0; i < chunks.length; i++) {
+                    chunks[i] = this._format(chunks[i], this._chunkFormatter);
+                }
+            }
+            this._chunkReceived(chunk);
         };
         this._socket.onclose = (event) => {
             console.log('WebSocket connection closed:', event, 'reconnecting ');
